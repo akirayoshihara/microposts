c64097bca4c6d3f7c2ad41bb6f4a78d4c3e54ce6
akirayoshihara <ahrasia@gmail.com>
2016-03-23 14:23:44 +0000

    submit1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319025723) do
+ActiveRecord::Schema.define(version: 20160323132336) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160319025723) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "age"
+    t.string   "comment"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
